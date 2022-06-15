bf23ae33244ae06362acbc6cdae1b4f00cd45d5b
Joey Estrada <joeyestradamusic@gmail.com>
2022-06-15 13:22:26 -0400

    added routes and functionality for /theaters

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,29 +1,26 @@
 const knex = require("../db/connection");
+const reduceProperties = require("../utils/reduce-properties");
+
+const reduceMovies = reduceProperties("theater_id", {
+  movie_id: ["movies", null, "movie_id"],
+  title: ["movies", null, "title"],
+  runtime_in_minutes: ["movies", null, "runtime_in_minutes"],
+  rating: ["movies", null, "rating"],
+  description: ["movies", null, "description"],
+  image_url: ["movies", null, "image_url"],
+  is_showing: ["movies", null, "is_showing"],
+  theater_id: ["movies", null, "theater_id"],
+});
 
 function list() {
-  return knex("movies_theaters as mt")
-    .join("movies as m", "mt.movie_id", "m.movie_id")
-    .join("theaters as t", "mt.theater_id", "t.theater_id")
-    .select("m.*", "mt.*")
+  return knex("theaters as t")
+    .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
+    .join("movies as m", "m.movie_id", "mt.movie_id")
+    .select("t.*", "m.*")
     .where({ "mt.is_showing": true })
-    .then(() => {
-
-    })
+    .then((data) => reduceMovies(data));
 }
 
 module.exports = {
   list,
 };
-
-// select *
-// from theaters t
-// join movies_theaters mt
-// on t.theater_id = mt.theater_id
-// join movies m
-// on m.movie_id = mt.movie_id
-// where mt.is_showing = true
-
-// .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
-//     .join("movies as m", "mt.movie_id", "m.movie_id")
-//     .select("t.*", "m.*", "mt.*")
-//     .where({"mt.is_showing": true})
