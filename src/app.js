1855878fa26fea701d6bd38cfed9b4eb89d63393
Joey Estrada <joeyestradamusic@gmail.com>
2022-06-17 10:30:08 -0400

    redeploy

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const errorHandler = require("./errors/errorHandler");
 // But I know this one!
 app.use(cors())
 
-// Still don't know what this does :D
+// Still don't know what this does
 app.use(express.json());
 
 // Routers
